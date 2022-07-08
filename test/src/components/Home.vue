09a48e893e411f14e0deb5e04b65902c3abffd5d
Jose Ramon Vieites Romay <jose.vieites@altia.es>
2022-07-08 19:13:53 +0200

    Clase 13

---
+++ test/src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+export default {
+  name: "home-component",
+  setup() {
+    onMounted(() => {
+      console.log("mounted!");
+    });
+  },
+};
+</script>
